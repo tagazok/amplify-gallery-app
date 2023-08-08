db8d0b1cdbb505284a14ba813d263b7606769621
tagazok <leplus.olivier@gmail.com>
2023-08-08 11:05:01 +0200

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "imagegalleryb3fdb188": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "imagegallery": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
